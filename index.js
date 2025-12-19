9e50a5c3dc70af536b664f018c23b6077496f737
nisola <jimohayomide20@gmail.com>
2025-12-19 11:18:58 +0100

    biology

--- index.js
+++ index.js
@@ -18,317 +18,208 @@ let timer;
 let playerName = "";
 
 const questions = [
-  { q: "Adisa had very good remarks on his report card", 
-    a: ["details", "design", "comment", "ideas"], 
-    correct: "comment" 
+  {
+    q: "The mode of nutrition in which digestion is extracellular is _____",
+    a: ["Saprophytic", "Parasitic", "Holophytic", "Holozoic"],
+    correct: "Saprophytic"
   },
-
-  { q: "We must work hard in order to surmount our difficulties", 
-    a: ["decrease", "overcome", "remove", "meet"], 
-    correct: "overcome" 
+  {
+    q: "Emergent trees are at _____ layer of the forest",
+    a: ["Upper", "Lower", "Canopy", "Understory"],
+    correct: "Upper"
   },
-
-  { q: "The government gets most of its revenue from tax and custom dues.", 
-    a: ["riches", "fees", "income", "support"], 
-    correct: "income" 
+  {
+    q: "The principle of survival of the fittest is regarded as _____",
+    a: ["Darwinism", "Lamarckism", "Mendelism", "Naturalism"],
+    correct: "Darwinism"
   },
-
-  { q: "The monitor was told to look into the cause of the confusion in the classroom.", 
-    a: ["investigate", "punish", "spy on", "watch"], 
-    correct: "investigate" 
+  {
+    q: "An example of Estuarine habitat is _____",
+    a: ["Pond", "Lagoon", "Ocean", "Lake"],
+    correct: "Lagoon"
   },
-
-  { q: "The workers protested about the cut in their wages", 
-    a: ["decrease", "lateness", "amount", "gap"], 
-    correct: "decrease" 
+  {
+    q: "An edaphic factor of growth of plants is _____",
+    a: ["Soil PH", "Temperature", "Light intensity", "Humidity"],
+    correct: "Soil PH"
   },
-
-  { q: "She found a naira note on the field", 
-    a: ["hid", "discovered", "lost", "dropped"], 
-    correct: "discovered" 
+  {
+    q: "What is the relationship between cattle and cattle egret?",
+    a: ["Commensalism", "Mutualism", "Parasitism", "Competition"],
+    correct: "Commensalism"
   },
-
-  { q: "The man asked his wife to buy expensive materials for her dresses", 
-    a: ["beautiful", "gay", "costly", "many"], 
-    correct: "costly" 
+  {
+    q: "Inbreed crossing refers to the breeding of _____",
+    a: ["Unrelated species", "Closely related species", "Species with different habitats", "Genetically modified organisms"],
+    correct: "Closely related species"
   },
-
-  { q: "Mr Subomi wants to give up his appointment", 
-    a: [
-      "transfer his appointment to another",
-      "resign his appointment",
-      "keep the appointment for the son",
-      "share his appointment with others"
-    ],
-    correct: "resign his appointment"
+  {
+    q: "A man of AS genotype marries a woman of AS genotype, what is the probability of having a sickle cell anemia offspring?",
+    a: ["0%", "25%", "50%", "100%"],
+    correct: "25%"
   },
-
-  { q: "I passed my entrance examination ……….. unfortunately, I was not accepted by the university", 
-    a: ["and", "because", "for", "but"], 
-    correct: "but" 
+  {
+    q: "The effect of the lack of vascular roots of a bryophyte is _____",
+    a: ["Reduced water absorption", "Efficient nutrient transport", "Unlimited growth potential", "Improved structural support"],
+    correct: "Reduced water absorption"
   },
-
-  { q: "The only witness to the theft was so shocked that he couldn't describe exactly ……..", 
-    a: [
-      "what the thief looked like",
-      "how the thief looked like",
-      "that the thief looked like",
-      "which the thief looked like"
-    ],
-    correct: "what the thief looked like"
+  {
+    q: "The national park of Nigeria is located at _____",
+    a: ["Ogun state", "Lagos state", "Cross River State", "Yobe State"],
+    correct: "Cross River State"
   },
-
-  { q: "The lady accompanied by her two friends …………", 
-    a: ["has arrived", "have arrived", "have been arriving", "is being arrived"], 
-    correct: "has arrived" 
+  {
+    q: "A criminal can be traced through which of the following?",
+    a: ["Blood group", "Fingerprint", "Genotype", "Shoe size"],
+    correct: "Fingerprint"
   },
-
-  { q: "When one lecturer had an accident last semester I often go to the hospital …………*", 
-    a: ["for visit", "to be visiting", "to visit", "visit"], 
-    correct: "to visit" 
+  {
+    q: "The vegetative part of a plant is _____",
+    a: ["Leaves", "Flowers", "Stems", "Root"],
+    correct: "Leaves"
   },
-
-  { q: "Ade was unhappy after the race …….. he failed to get a medal", 
-    a: ["because", "since", "but", "for"], 
-    correct: "because" 
+  {
+    q: "The immature fruits of opium poppy are _____",
+    a: ["Capsules", "Berries", "Drupes", "Achenes"],
+    correct: "Capsules"
   },
-
-  { q: "This is the lady ……. stole my car", 
-    a: ["who", "which", "whom", "would have"], 
-    correct: "who" 
+  {
+    q: "Pneumatophores developed by white mangroves are used for _____",
+    a: ["Gaseous exchange", "Anchoring the plant in the soil", "Storing water", "Photosynthesis"],
+    correct: "Gaseous exchange"
   },
-
-  { q: "If you were around, I …….. happy", 
-    a: ["would be", "would have been", "have been", "was"], 
-    correct: "would be" 
+  {
+    q: "Which type of cartilage is found in the external ear, epiglottis and larynx?",
+    a: ["Hyaline cartilage", "Fibrocartilage", "Elastic cartilage", "None of the above"],
+    correct: "Elastic cartilage"
   },
-
-  { q: "A was bedridden for a week, Akeem was too ……… to assist", 
-    a: ["smart", "gently", "head", "reluctant"], 
-    correct: "smart" 
+  {
+    q: "Which of these lacks a true nucleus?",
+    a: ["Amoeba", "Moss", "Rhizopus", "Bacteria"],
+    correct: "Bacteria"
   },
-
-  { q: "While I was gone who ……… here", 
-    a: ["came", "comes", "have come", "has come"], 
-    correct: "came" 
+  {
+    q: "Arthropods are distinguished from vertebrates because they possess _____",
+    a: ["Bones", "Chitin", "Feathers", "Scales"],
+    correct: "Chitin"
   },
-
-  { q: "Good children assist their mothers ……….*", 
-    a: ["cooked", "to cook", "to be cooking", "in cooking"], 
-    correct: "in cooking" 
+  {
+    q: "Digested food in the body is transported by _____",
+    a: ["White blood cells (WBC)", "Red blood cells (RBC)", "Platelets", "Plasma"],
+    correct: "Plasma"
   },
-
-  { q: "The football team supporters ………. wildly when a goal was scored", 
-    a: ["cheered", "groaned", "wept", "shouted"], 
-    correct: "cheered" 
+  {
+    q: "The breeding of related species is known as _____",
+    a: ["Inbreeding", "Self-breeding", "Outbreeding", "Cross breeding"],
+    correct: "Inbreeding"
   },
-
-  { q: "When the fire alarm came on, we ……….. building", 
-    a: ["rushed", "walked", "strolled", "crawled"], 
-    correct: "rushed" 
+  {
+    q: "Unicellular organisms are mostly found among _____",
+    a: ["Plants", "Animals", "Microorganisms", "Viruses"],
+    correct: "Microorganisms"
   },
-
-  { q: "The party has …….. the country for a long time", 
-    a: ["tormented", "promised", "ruled", "lied"], 
-    correct: "ruled" 
+  {
+    q: "The aphotic zone is characterized by _____",
+    a: ["Absence of light", "Constant sunlight", "High temperature", "High oxygen levels"],
+    correct: "Absence of light"
   },
-
-  { q: "The father died only ……….. arrived from Russia", 
-    a: ["a", "those", "an", "the"], 
-    correct: "a" 
+  {
+    q: "Plants in a mangrove swamp have _____",
+    a: ["Spines", "Breathing roots", "Thick stems", "Large leaves"],
+    correct: "Breathing roots"
   },
-
-  { q: "A long time was expended before Sisi Caro could settle …….. the price", 
-    a: ["in", "to", "on", "with"], 
-    correct: "on" 
+  {
+    q: "The highest energy level is found in _____",
+    a: ["Carbohydrates", "Proteins", "Fats and oil", "Water"],
+    correct: "Fats and oil"
   },
-
-  { q: "When the children heard the ………. of their father’s car, they all ran", 
-    a: ["horn", "humming", "sound", "brake"], 
-    correct: "horn" 
+  {
+    q: "What is the relationship between zebra and lion?",
+    a: ["Parasitism", "Predation", "Symbiosis", "Commensalism"],
+    correct: "Predation"
   },
-
-  { q: "In the lecture hall the lecturer gave the ………….. to the students", 
-    a: ["lecture", "dialogue", "warning", "talk"], 
-    correct: "lecture" 
+  {
+    q: "Which of the following is an example of genetic variation?",
+    a: ["Person's hair color", "A person's haircut", "A person's clothing style", "A person's shoe size"],
+    correct: "Person's hair color"
   },
-
-  { q: "When the pupils heard the ………. they ran to their classes", 
-    a: ["bell", "head master's voice", "sound of rain", "ringing"], 
-    correct: "bell" 
+  {
+    q: "Which of the following is an example of continuous variation?",
+    a: ["Height in humans", "Blood type in humans", "Flower color in plants", "Fur pattern in animals"],
+    correct: "Height in humans"
   },
-
-  { q: "The priest gave ………. in the church", 
-    a: ["dialogue", "counselling", "sermon", "advice"], 
-    correct: "sermon" 
+  {
+    q: "The mixture of fresh water and sea water is called _____",
+    a: ["Brackish water", "Saline water", "Potable water", "Fresh water"],
+    correct: "Brackish water"
   },
-
-  { q: "Olumide is absent …………. the lecture of today", 
-    a: ["from", "of", "over", "in"], 
-    correct: "from" 
+  {
+    q: "A tree growing with broad and ever-green leaves is likely located in _____",
+    a: ["A tropical rainforest", "A desert biome", "A tundra ecosystem", "A deciduous forest"],
+    correct: "A tropical rainforest"
   },
-
-  { q: "The man in black suit should ………. the horse", 
-    a: ["mount", "sit", "in", "by"], 
-    correct: "mount" 
+  {
+    q: "Which inorganic material is needed for photosynthesis?",
+    a: ["Oxygen", "Carbon dioxide", "Nitrogen", "Iron"],
+    correct: "Carbon dioxide"
   },
-
-  { q: "I was asked to share the ……….. among my siblings", 
-    a: ["ice cream", "horse", "car", "phone"], 
-    correct: "ice cream" 
+  {
+    q: "What is the excretory organ of the earthworm?",
+    a: ["Kidneys", "Malpighian tubules", "Nephridia", "Lungs"],
+    correct: "Nephridia"
   },
-
-  { q: "I ………. to Uyo tomorrow", 
-    a: ["probably", "shall probably", "have probably gone", "were probably going"], 
-    correct: "shall probably" 
+  {
+    q: "Opium poppy produces _____",
+    a: ["Nicotine", "Quinine", "Morphine", "Caffeine"],
+    correct: "Morphine"
   },
-
-  
-  { q: "They all agree that John was a fast runner", 
-    a: ["cheered", "denied", "said", "shouted"], 
-    correct: "denied" 
+  {
+    q: "Agglutination occurs when blood is passed from blood group _____",
+    a: ["A to AB", "AB to A", "O to AB", "O to A"],
+    correct: "AB to A"
   },
-
-  { q: "Last year our well was empty", 
-    a: ["few", "full", "shallow", "low"], 
-    correct: "full" 
+  {
+    q: "The part of the fish's body that is sensitive to vibration is _____",
+    a: ["Lateral fin", "Dorsal fin", "Gills", "Tail fin"],
+    correct: "Lateral fin"
   },
-
-  { q: "Solomon did not feel comfortable because the coat was rough", 
-    a: ["light", "cheap", "smooth", "delicate"], 
-    correct: "smooth" 
+  {
+    q: "The bird (Woodpecker) performs which activity?",
+    a: ["Captures fast-moving prey in flight", "Probes for insects hidden in tree bark", "Sips nectar", "Catches fish in shallow water"],
+    correct: "Probes for insects hidden in tree bark"
   },
-
-  { q: "Olu is a very good friend on..... I can rely", 
-    a: ["where", "which", "whom", "who"], 
-    correct: "whom" 
+  {
+    q: "Which type of cartilage provides a smooth surface for joint movement and is commonly found in the ribs, trachea and bronchi?",
+    a: ["Fibrocartilage", "Elastic cartilage", "Hyaline cartilage", "Articular cartilage"],
+    correct: "Hyaline cartilage"
   },
-
-  { q: "Thought...... the pencil by now", 
-    a: ["to sent", "to be sent", "to have send", "to have sent"], 
-    correct: "to have sent" 
+  {
+    q: "The pectoral fin is the part labelled _____",
+    a: ["IV", "VII", "V", "III"],
+    correct: "VII"
   },
-
-  { q: "Janet's fever was so acute that she … an injection", 
-    a: ["must have", "ought to have", "had to have", "had"], 
-    correct: "had to have" 
+  {
+    q: "Most cells in higher animals retain their power of cell division except _____",
+    a: ["Lymphocytes", "Malpighian cells", "Nerve cells", "Germ cells"],
+    correct: "Nerve cells"
   },
-
-  { q: "I know of an elephant... has only three legs", 
-    a: ["who", "which", "whom", "of which"], 
-    correct: "which" 
+  {
+    q: "The function of ribosomes in cells is _____",
+    a: ["Protein synthesis", "Starch synthesis", "Lipid storage", "Transport of materials"],
+    correct: "Protein synthesis"
   },
-
-  { q: "Yemisi, this is not my keys", 
-    a: ["whose", "of which", "who's", "whom"], 
-    correct: "whose" 
+  {
+    q: "The membrane surrounding the vacuole in plants is called _____",
+    a: ["Plasmalemma", "Tonoplast", "Sarcolemma", "Nuclear membrane"],
+    correct: "Tonoplast"
   },
-
-  { q: "There was no tomatoes in the supermarket … Ronke bought some pepper", 
-    a: ["unless", "so", "whereas", "since"], 
-    correct: "so" 
+  {
+    q: "A man of AS genotype marries a woman of AS genotype, what is the probability of having a sickle cell anemia offspring?",
+    a: ["0%", "25%", "50%", "100%"],
+    correct: "25%"
   },
-
-  { q: "The security vehicle raced ….. full speed", 
-    a: ["on", "with", "at", "in"], 
-    correct: "at" 
-  },
-
-  { q: "William loves reading love story books to take his mind … his worries", 
-    a: ["from", "out", "away", "off"], 
-    correct: "off" 
-  },
-
-  { q: "They are always quarreling ……….", 
-    a: ["themselves", "myself", "one another", "each of them"], 
-    correct: "one another" 
-  },
-
-  { q: "David couldn't have said a thing like that ……..", 
-    a: ["did he?", "could he?", "would he?", "can he?"], 
-    correct: "could he?" 
-  },
-
-  { q: "If he had known, he wouldn't have come …….*", 
-    a: ["wasn't?", "would he?", "couldn't he?", "wouldn't?"], 
-    correct: "would he?" 
-  },
-
-  { q: "Sack of workers could be the ……… of the workers' protest", 
-    a: ["curse", "course", "oparse", "cause"], 
-    correct: "cause" 
-  },
-
-  { q: "Neither James nor Stella felt happy …….. the incident", 
-    a: ["by", "in", "about", "for"], 
-    correct: "about" 
-  },
-
-  { q: "Anybody would be impressed …….. such impressive performance", 
-    a: ["for", "at", "with", "from"], 
-    correct: "with" 
-  },
-
-  { q: "Morunfolu wanted to show …….. with her necklace", 
-    a: ["on", "of", "back", "over"], 
-    correct: "of" 
-  },
-
-  { q: "Ajose and Emmanuel …….. the work yesterday", 
-    a: ["finished", "finish", "shall finish", "would finish"], 
-    correct: "finished" 
-  },
-
-  { q: "A large number of spectators … at the match", 
-    a: ["were been", "were", "was", "have been"], 
-    correct: "were" 
-  },
-
-  { q: "Tom and his wife ......... killed in an accident", 
-    a: ["were", "was", "were been", "had been"], 
-    correct: "were" 
-  },
-
-  { q: "My mother instructed me that I must ……… before breakfast", 
-    a: ["bath", "have my bathe", "take my bath", "bath myself"], 
-    correct: "bath" 
-  },
-
-  { q: "Take your umbrella because it ......", 
-    a: ["is raining", "wanted to rain", "had rained", "was raining"], 
-    correct: "is raining" 
-  },
-
-  { q: "When I heard the deafening sound I .......... under the bed", 
-    a: ["hidden", "had hid", "hid", "am hiding"], 
-    correct: "hid" 
-  },
-
-  { q: "Babatope wishes he …….. a such man", 
-    a: ["am being", "was", "were", "am"], 
-    correct: "was" 
-  },
-
-  { q: "Janet sat for the examination ……… she had malaria", 
-    a: ["while", "since", "though", "why"], 
-    correct: "though" 
-  },
-
-  { q: "Amina wore a man's hat ………. looked very funny", 
-    a: ["that", "who", "which", "whose"], 
-    correct: "that" 
-  },
-
-  { q: "When Ade came in, he sat on a chair and ……… for a drink", 
-    a: ["was asking", "asked", "will ask", "after"], 
-    correct: "asked" 
-  },
-
-  { q: "Please if you come .......... my brother in town, inform him", 
-    a: ["over", "across", "through", "after"], 
-    correct: "across" 
-  }
 ];
+
 startBtn.addEventListener("click", startQuiz);
 nextBtn.addEventListener("click", nextQuestion);
 function startQuiz() {
@@ -346,7 +237,7 @@ function startQuiz() {
   quizBox.classList.remove("hide");
   currentQuestion = 0;
   score = 0;
-  timeLeft = 12;
+  timeLeft = 20;
   showQuestion();
   startTimer();
 
@@ -386,7 +277,7 @@ function nextQuestion() {
   currentQuestion++;
   if (currentQuestion < questions.length) {
     nextBtn.classList.add("hide");
-    timeLeft = 12;
+    timeLeft = 20;
     showQuestion();
     startTimer();
   } else {
@@ -412,7 +303,7 @@ function showResult() {
   scoreDisplay.textContent = score;
   playerNameDisplay.textContent = playerName;
   // Reset the entire resultBox content (except fixed elements like score title if any)
-  resultBox.innerHTML = `<div class="name"> 
+  resultBox.innerHTML = ` 
     <h2>Quiz Completed 👍</h2>
     <p>Your Final Score: <span id="score">${score}</span> / ${questions.length}</p>
     <p>Thanks for playing <span class="wow">${playerName}!</span></p>
@@ -421,19 +312,16 @@ function showResult() {
   const categoryText = document.createElement("p");
   categoryText.classList.add("category");
 
-  if (score <= 20 ) {
+  if (score <= 15 ) {
     categoryText.textContent = "Poor 😢 – You need more practice OLODO!";
-  } else if (score <= 30) {
+  } else if (score <= 20) {
     categoryText.textContent = "Fair 🙂 – You can do better READ!";
-  } else if (score <= 40) {
+  } else if (score <= 29) {
     categoryText.textContent = "Good 👍 – Nice effort MORE EFFORT!";
-  } else if (score <= 50) {
+  } else if (score <= 35) {
     categoryText.textContent = "Excellent 🔥 – You really know your stuff BRAINBOX!";
-  } else if (score <= 55) {
-    categoryText.textContent = "Wow 🎯 – Genius level almost a GURU!";
-  }
-  else if (score <= 60) {
-    categoryText.textContent = "A Guru, take your flower";
+  } else if (score === 40) {
+    categoryText.textContent = "Wow 🎯 – Genius level GURU!";
   }
 
   resultBox.appendChild(categoryText);
@@ -449,18 +337,11 @@ function showResult() {
   resultBox.appendChild(playAgainBtn);
 
 }
-function shuffleQuestions() {
-  for (let i = questions.length - 1; i > 0; i--){
-    const j = Math.floor(Math.random() * (i + 1));
-    [questions[i], questions[j]] =[questions[j], questions[i]];
-  }
-}
 function restartQuiz() {
   clearInterval(timer);
 currentQuestion = 0;
 score = 0;
 timeLeft = 20;
-shuffleQuestions()
 startBtn.classList.add("hide");
 resultBox.classList.add("hide");
 quizBox.classList.remove("hide");
